20592ba5eb3c4ecdedfc615b1ab3169771fcaa56
PR3D4T0R08 <lennard.maretzki@gmail.com>
2022-02-22 21:16:51 +0100

    copied first try

---
+++ firsttry.py
@@ -0,0 +1,22 @@
+first_name = "Lennard"
+last_name = "Maretzki"
+full_name = first_name + " "+ last_name
+print("Hello "+ full_name)
+#print("Hello "+name)
+
+age = 19
+#age =age + 1
+#print(age)
+print("You are "+str(age)+ " years old.")
+
+height = 187
+print("your height is: "+ str(height)+ " cm")
+#print(type(height))
+
+human = False
+#print(human)
+#print(type(human))
+print("Are you a Human?: " + str(human))
+
+Cyborg = True
+print("Are you a Cyborg?: " + str(Cyborg))
